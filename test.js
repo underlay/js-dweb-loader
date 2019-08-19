d447876c6e8b2393159304f639526069ce9d00e7
Joel Gustafson <joelg@mit.edu>
2019-08-19 12:19:03 -0400

    bumped version

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
-const IPFS = require("ipfs-http-client")
-const { Buffer } = require("ipfs-http-client")
-
 const jsonld = require("jsonld")
+const IPFS = require("ipfs-http-client")
+const { Buffer } = IPFS
 
 const createDocumentLoader = require("./index.js")
 
@@ -15,39 +14,35 @@ const context = {
 	prov: "http://www.w3.org/ns/prov#",
 }
 
-ipfs.dag.put(
-	context,
-	{ format: "dag-cbor", hashAlg: "sha2-256" },
-	(err, cid) => {
-		const contextUri = `dweb:/ipld/${cid.toBaseEncodedString()}`
-		console.log("context:", contextUri)
-		const doc = {
-			"@context": contextUri,
-			"prov:wasAttributedTo": {
+ipfs.dag.put(context).then(cid => {
+	const contextUri = `dweb:/ipld/${cid.toBaseEncodedString()}`
+	console.log("context:", contextUri)
+	const doc = {
+		"@context": contextUri,
+		"prov:wasAttributedTo": {
+			"@type": "schema:Person",
+			"schema:name": "Eve",
+		},
+		"@graph": {
+			"@type": "schema:Person",
+			"schema:name": "Alice",
+			"schema:knows": {
 				"@type": "schema:Person",
-				"schema:name": "Eve",
+				"schema:name": "Bob",
 			},
-			"@graph": {
-				"@type": "schema:Person",
-				"schema:name": "Alice",
-				"schema:knows": {
-					"@type": "schema:Person",
-					"schema:name": "Bob",
-				},
-			},
-		}
-
-		const bytes = Buffer.from(JSON.stringify(doc))
-		ipfs.add(bytes, (err, [{ hash }]) => {
-			const docUri = `ipfs://${hash}`
-			console.log("document:", docUri)
-			jsonld
-				.expand(docUri)
-				.then(expanded => jsonld.compact(expanded, contextUri))
-				.then(result => {
-					console.log(JSON.stringify(result, null, "  "))
-					process.exit()
-				})
-		})
+		},
 	}
-)
+
+	const bytes = Buffer.from(JSON.stringify(doc))
+	ipfs.add(bytes).then(([{ hash }]) => {
+		const docUri = `ipfs://${hash}`
+		console.log("document:", docUri)
+		jsonld
+			.expand(docUri)
+			.then(expanded => jsonld.compact(expanded, contextUri))
+			.then(result => {
+				console.log(JSON.stringify(result, null, "  "))
+				process.exit()
+			})
+	})
+})
